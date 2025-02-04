9e20447b34721d6462d2b618924e572f364b5f19
Frank Edward Sanchez Dominguez <fsanchez@csleab-domain.lan>
2025-02-04 08:10:42 -0400

    Se creo Modulos y dependencias de DEV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PacienteModule } from './paciente/paciente.module';
+import { FacturacionModule } from './facturacion/facturacion.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PacienteModule, FacturacionModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
